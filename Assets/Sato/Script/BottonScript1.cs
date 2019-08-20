43bcc517d20555874c45f92ebd1a91305dd84d02
pororosan <satoyuki0913@gmail.com>
2019-08-20 11:54:21 +0900

    テスト3

--- Assets/Sato/Script/BottonScript1.cs
+++ Assets/Sato/Script/BottonScript1.cs
@@ -1,27 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class BottonScript : MonoBehaviour
-{
-    bool bullet1 = true;
-    bool bullet2 = false;
+//public class BottonScript : MonoBehaviour
+//{
+//    bool bullet1 = true;
+//    bool bullet2 = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+//    // Start is called before the first frame update
+//    void Start()
+//    {
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
+//    }
 
-    public void ButtonClick1()
-    {
-        bullet1 = true;
-        bullet2 = false;
-    }
-}
+//    public void ButtonClick1()
+//    {
+//        bullet1 = true;
+//        bullet2 = false;
+//    }
+//}
